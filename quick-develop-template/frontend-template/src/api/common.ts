05b945deadbe84901dedd7d879c0e7df740be75c
linxun1209 <3056726345@qq.com>
2024-01-07 23:35:06 +0800

    添加上传文件的枚举类

--- quick-develop-template/frontend-template/src/api/common.ts
+++ quick-develop-template/frontend-template/src/api/common.ts
@@ -3,17 +3,20 @@ import { BaseResponse } from "@/model/BaseResponse";
 
 /** 获取邮箱验证码 */
 export const GetMailCode = async (targetEmail: string) => {
-  return await http.post<string, BaseResponse<string>>("/common/getMailCode", { data: targetEmail });
+  return await http.post<string, BaseResponse<string>>("/common/getMailCode", {
+    data: targetEmail
+  });
 };
 
 /** 上传图片 */
 export const UploadImage = async (forms: FormData) => {
-  return await http.post<FormData, BaseResponse<string>>("/file/upload", { data: forms },{
-    headers:{
-      "Content-Type": "multipart/form-data"
+  return await http.post<FormData, BaseResponse<string>>(
+    "/file/upload",
+    { data: forms },
+    {
+      headers: {
+        "Content-Type": "multipart/form-data"
+      }
     }
-  });
+  );
 };
-
-
-
